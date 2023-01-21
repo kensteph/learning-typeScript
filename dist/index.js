0508e5d3ba7be24b567cbad6502a0d964a6988a8
Kender ROMAIN <kenderromain@gmail.com>
2023-01-21 17:48:32 -0400

    Create laun.json for debuggin purposes and add preLaunchTask setting

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let myTuple;
+let age = 35;
+if (age > 40)
+    age += 10;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let myTuple: [boolean,string,number];
+
+let age : number = 35;
+if(age > 40 ) age +=10;
